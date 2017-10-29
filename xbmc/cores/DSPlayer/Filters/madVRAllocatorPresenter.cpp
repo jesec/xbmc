11523e7b8eb0bacf6d0d5b0e10f74453b8e2fa64
aracnoz <aracnoz@yahoo.it>
2017-10-29 17:30:43 +0100

    don't use kodi ChangeResolution registry hack with Windows Fall Creators Update build

--- xbmc/cores/DSPlayer/Filters/madVRAllocatorPresenter.cpp
+++ xbmc/cores/DSPlayer/Filters/madVRAllocatorPresenter.cpp
@@ -620,7 +620,9 @@ bool CmadVRAllocatorPresenter::SetResolutionInternal(const RESOLUTION res, bool 
     bool bResChanged = false;
 
     // Windows 8 refresh rate workaround for 24.0, 48.0 and 60.0 Hz
-    if (CSysInfo::IsWindowsVersionAtLeast(CSysInfo::WindowsVersionWin8) && (res_info.fRefreshRate == 24.0 || res_info.fRefreshRate == 48.0 || res_info.fRefreshRate == 60.0))
+    if (CSysInfo::IsWindowsVersionAtLeast(CSysInfo::WindowsVersionWin8) 
+      && !CSysInfo::IsWindowsVersionAtLeast(CSysInfo::WindowsVersionWin10_FCU) 
+      && (res_info.fRefreshRate == 24.0 || res_info.fRefreshRate == 48.0 || res_info.fRefreshRate == 60.0))
     {
       CLog::Log(LOGDEBUG, "%s : Using Windows 8+ workaround for refresh rate %d Hz", __FUNCTION__, static_cast<int>(res_info.fRefreshRate));
 
